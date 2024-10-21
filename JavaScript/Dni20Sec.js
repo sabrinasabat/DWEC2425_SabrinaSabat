73090f0b056094ecd36c305874df36ac655983c9
sabrinasabat <sabrinasabats@gmail.com>
2024-10-21 11:17:02 +0200

    commit 21.10.24

--- JavaScript/Dni20Sec.js
+++ JavaScript/Dni20Sec.js
@@ -1,27 +1,26 @@
 function soliciteDni() {
+        var letrasDni = [];
 
-    var letrasDni = [];
-
-    do{
-        var dni = window.prompt("Digite el número de su DNI. (Para salir digite -1)")
-        if (dni!=="-1"){ // si el usuario digita -1 se detiene.
-            var num = dni%23;
-            var letra = ["TRWAGMYFPDXBNJZSQVHLCKE"];
-            for(var i=0; i<num; i++){
-                if(num===letra[i]){
-                    letrasDni.push(letra); //poniendo la letra en el array
+        do{
+            var dni = window.prompt("Digite el número de su DNI. (Para salir digite -1)")
+            if (dni!=="-1"){ // si el usuario digita -1 se detiene.
+                var num = dni%23;
+                var letra = ["TRWAGMYFPDXBNJZSQVHLCKE"];
+                for(var i=0; i<num; i++){
+                    if(num===letra[i]){
+                        letrasDni.push(letra); //poniendo la letra en el array
+                    }
                 }
-            }
             //var letra = dni.charAt(8); //pillando la ultima letra
 
+            }
+
+
+        } while(dni!=="-1");
+
+        for(var j=0; j<letrasDni.length;j++){
+            console.log("Las letras de todos los DNIs digitados son: "+letrasDni[j]);
         }
-
-
-    } while(dni!=="-1");
-
-    for(var j=0; j<letrasDni.length;j++){
-        console.log("Las letras de todos los DNIs digitados son: "+letrasDni[j]);
-    }
 
 
     /*String.prototype.letraNif=function () {
