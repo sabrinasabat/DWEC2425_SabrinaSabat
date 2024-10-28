6e7f282bc6fe57bdb1940f00754887d8b19771b4
sabrinasabat <sabrinasabats@gmail.com>
2024-10-28 09:53:04 +0100

    commit 28.10.24

--- JavaScript/u3e6_circulo.js
+++ JavaScript/u3e6_circulo.js
@@ -12,17 +12,22 @@
 * Como datos de muestra, si metes 5, deberías obtener aproximadamente: 5 / 10 / 31,41 / 78,54 / 314,15 / 523,59*/
 function circulo(){
 
-    var n = window.prompt("Digite el valor del radio: ");
+    var radio = parseFloat(window.prompt("Digite el valor del radio: "));
 
-    var radio = n/(2*Math.PI);
     var diametro = radio*2;
-    var perimetroCirc = 2*Math.PI*radio;
-    var areaCirc = Math.PI*Math.pow(radio, 2);
+    var perimetroCirc = Math.floor(2*Math.PI*radio);
+    var areaCirc = Math.floor(Math.PI*Math.pow(radio, 2));
+    var areaEsf = Math.floor(4*Math.PI*Math.pow(radio, 2));
+    var volumenEsf = Math.floor((4/3)*Math.PI*Math.pow(radio,3));
 
-
-    window.alert("El valor del radio: "+radio);
+    window.alert("El valor del radio: "+radio+" cm");
     window.alert("El valor del diametro: "+diametro);
-    window.alert("El valor del perimetro de la circunferencia: "+perimetroCirc);
-    window.alert("El valor de la área del círculo: "+areaCirc);
+    window.alert("El valor del perimetro de la circunferencia: "+perimetroCirc+" cm");
+    window.alert("El valor de la área del círculo: "+areaCirc+" cm");
+    window.alert("El área de la esfera: "+areaEsf+" cm");
+    window.alert("El valor del volumen de la esfera es: "+volumenEsf+" cm");
 
+    const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+    const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+    const sphere = new THREE.Mesh( geometry, material ); scene.add( sphere );
 }
